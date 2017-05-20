eb30179fee1e8d4fa37ed6f6e17df4029fdff27b
ssurendran <sujith.surendran@armor.com>
2017-05-20 20:26:49 +0530

    Initial project creation

---
+++ Algorithms/Algorithms.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
